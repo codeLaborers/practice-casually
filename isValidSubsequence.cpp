464ad6c8cd64b1bbf187e6c9e53bbb867aac9f75
codeLaborers <1183965998@qq.com>
2022-09-10 21:10:26 +0800

    nothing

---
+++ isValidSubsequence.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+
+bool isValidSubsequence(vector<int> array, vector<int> sequence){
+	int arrIdx=0;
+	int seqIdx=0;
+	while(arrIdx<array.size() && seqIdx<sequence.size()){
+		if(array[arrIdx]==sequence[seqIdx]){
+			seqIdx++;
+		}
+		arrIdx++;
+	}
+	return seqIdx==sequence.size();
+}
