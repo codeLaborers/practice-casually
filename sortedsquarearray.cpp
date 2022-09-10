1a336a9cf29d3de90e511c039a742150d8d175f5
codeLaborers <1183965998@qq.com>
2022-09-10 21:07:12 +0800

    nothing

---
+++ sortedsquarearray.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> sortedSquaredArray(vector<int> array){
+	vector<int> sortedSquares(array.size(),0);
+	for(int idx=0;idx<array.size();idx++){
+		int value=array[idx];
+		sortedSquares[idx]=value*value;
+	}
+	sort(sortedSquares.begin(),sortedSquares.end());
+	return sortedSquares;
+}
