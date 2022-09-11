7581fb8aef95712f6d1a5c3869fa6976b4c7eb18
codeLaborers <1183965998@qq.com>
2022-09-11 10:47:25 +0800

    nothing

---
+++ nonConstructiblechange.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int nonConstructibleChange(vector<int> coins){
+	sort(coins.begin(), coins.end());
+	int currentChangeCreated=0;
+	for(int coin:coins){
+		if(coin>currentChangeCreated+1){
+			return currentChangeCreated+1;
+		} 
+		currentChangeCreated+=coin;
+	}
+	return currentChangeCreated+1;
+}
